1f7eda60168b7d334590699619347e4b66e1165d
Sean T. Allen <sean@seantallen.com>
2020-08-27 22:40:46 -0400

    Smart error printing!

--- a.py
+++ a.py
@@ -4,10 +4,15 @@ print(os.path.realpath(__file__))
 
 from inspect import currentframe, getframeinfo
 
-frameinfo = getframeinfo(currentframe())
+def print_error(message, frame=currentframe()):
+  frameinfo = getframeinfo(frame)
+  print("::error file=" + os.path.realpath(frameinfo.filename) + ",line=" + str(frameinfo.lineno) + "::" + message) 
 
-print(os.path.realpath(frameinfo.filename), frameinfo.lineno)
-
-print("::error file=" + os.path.realpath(frameinfo.filename) + ",line=" + str(frameinfo.lineno) + "::Error here!")
-
-print("error file=" + os.path.realpath(frameinfo.filename) + ",line=" + str(frameinfo.lineno))
+print("hi")
+print_error("O no! error on line 12")
+print_error("And another!")
+#print(os.path.realpath(frameinfo.filename), frameinfo.lineno)
+#
+#print("::error file=" + os.path.realpath(frameinfo.filename) + ",line=" + str(frameinfo.lineno) + "::Error here!")
+#
+#print("error file=" + os.path.realpath(frameinfo.filename) + ",line=" + str(frameinfo.lineno))
